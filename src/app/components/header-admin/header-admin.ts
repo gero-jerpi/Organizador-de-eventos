d0985b2316d377648a251e48227ed705a5a4b188
candeflres <soflor51402@gmail.com>
2025-11-14 01:10:01 -0300

    header d admin y user
    hice el header de admin y user... espero

--- src/app/components/header-admin/header-admin.ts
+++ src/app/components/header-admin/header-admin.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-admin',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header-admin.html',
   styleUrl: './header-admin.css',
 })
 export class HeaderAdmin {
+  // ABRIR Y CERRAR MENU
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
--- src/app/components/header-user/header-user.ts
+++ src/app/components/header-user/header-user.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-user',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header-user.html',
   styleUrl: './header-user.css',
 })
 export class HeaderUser {
+  // ABRIR Y CERRAR MENU
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
