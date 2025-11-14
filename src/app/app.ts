cba015c15931b1587918b7afdd270b4226c2698f
candeflres <soflor51402@gmail.com>
2025-11-14 02:21:47 -0300

    footer y form login

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,10 @@ import { HeaderUser } from './components/header-user/header-user';
 import { HeaderAdmin } from './components/header-admin/header-admin';
 import { Register } from './pages/user/register/register';
 import { Login } from './pages/user/login/login';
-
+import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Register, Login, HeaderUser],
+  imports: [RouterOutlet, Register, Login, HeaderUser, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
