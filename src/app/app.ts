b0075eba93c493c8a2b36533ed91db54001d73a5
candeflres <soflor51402@gmail.com>
2025-11-14 01:13:39 -0300

    me falto el app apra el header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderUser } from "./components/header-user/header-user";
-import { Register } from "./pages/user/register/register";
-import { Login } from "./pages/user/login/login";
+import { HeaderUser } from './components/header-user/header-user';
+import { HeaderAdmin } from './components/header-admin/header-admin';
+import { Register } from './pages/user/register/register';
+import { Login } from './pages/user/login/login';
 
 @Component({
   selector: 'app-root',
+<<<<<<< Updated upstream
   imports: [RouterOutlet],
+=======
+  imports: [RouterOutlet, Register, Login, HeaderUser],
+>>>>>>> Stashed changes
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Organizador-de-eventos');
